00439ac93aa5e774369f3276460d80b3c84fa4d7
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-09-08 14:02:02 +0330

    add video

--- src/PictureComponent.tsx
+++ src/PictureComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import { useState, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
 
 export default function PictureComponent() {
