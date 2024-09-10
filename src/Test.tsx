fdf91ff27af23326bf4ca5b056571c3b11381c07
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-09-10 17:15:58 +0330

    test11

--- src/Test.tsx
+++ src/Test.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 export default function Test() {
   return (
-    <div className="tContainer">
-      {[...Array(200)].map((_, index) => (
-        <div className="tItem" key={index}>
-          {index + 1}
-        </div>
-      ))}
+    <div className="modal">
+      <div className="tContainer">
+        {[...Array(200)].map((_, index) => (
+          <div className="tItem" key={index}>
+            {index + 1}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
